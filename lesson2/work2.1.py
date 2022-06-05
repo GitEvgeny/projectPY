962fce3aee49790914eb448ce29b9d69837eb14b
Evgeny <PetrovE2@yandex.ru>
2022-06-05 14:00:57 +0800

    Урок 2. Задача 1

--- lesson2/work2.1.py
+++ lesson2/work2.1.py
@@ -1,1 +1,17 @@
-#
+# Задача 1
+
+my_list = [-1, False, 2, 4.8, 'строка', True, 99]
+print("Первый элемент списка, имеет тип:")
+print(type(my_list[0]))
+print("Второй элемент списка, имеет тип:")
+print(type(my_list[1]))
+print("Третий элемент списка, имеет тип:")
+print(type(my_list[2]))
+print("Четвёртый элемент списка, имеет тип:")
+print(type(my_list[3]))
+print("Пятый элемент списка, имеет тип:")
+print(type(my_list[4]))
+print("Шестой элемент списка, имеет тип:")
+print(type(my_list[5]))
+print("Седьмой элемент списка, имеет тип:")
+print(type(my_list[6]))
